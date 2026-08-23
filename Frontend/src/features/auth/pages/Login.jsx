6a8aed2c0c5951255df3f3c9f6707df346aa1f56
Escape <fardeenhasan57@gmail.com>
2026-08-24 00:06:37 +0530

    login page

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+
+                <form>
+
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type = "email" id='email' name="email" placeholder="Enter email address" ></input>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id='password' name="password" placeholder="Enter password" ></input>
+                    </div>
+
+                    <button className='button primary-button' >Login</button>
+                </form>
+            </div>
+    
+
+
+        </main>
     )
 }
 
