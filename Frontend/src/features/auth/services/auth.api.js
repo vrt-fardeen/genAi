319cf1603793e9c12310f2b3648dfeef13922941
Escape <fardeenhasan57@gmail.com>
2026-08-27 22:59:14 +0530

    started with axios

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -16,3 +16,19 @@ export async function register({username, email, password}) {
         console.log(err)
     }
 }
+
+export async function login({email, password}) {
+
+    try{
+
+        const response = await axios.post('http://localhost:3000/api/auth/login',{
+            email, password
+        }, {withCredentials:true})
+
+        return response.data
+
+    }catch(err) {
+        console.log(err)
+    }
+
+}
