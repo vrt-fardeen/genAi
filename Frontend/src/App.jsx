da81e848a86142e830d438144996bbdfd2ed9c24
Escape <fardeenhasan57@gmail.com>
2026-08-28 00:16:26 +0530

    started with hooks

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import {RouterProvider} from "react-router";
 import {router} from "./app.routes.jsx";
+import { AuthProvider } from "./features/auth/auth.context.jsx";
 
 function App() {
   
   return (
-    <RouterProvider router={router} />
+
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    
   )
 }
 
