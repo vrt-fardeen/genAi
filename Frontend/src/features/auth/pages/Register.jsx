fac7530d7c5dc21c9a350c1755e91347299fe39d
Escape <fardeenhasan57@gmail.com>
2026-08-27 22:42:37 +0530

    added services folder

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {useNavigate, Link} from "react-router";
+import { Link} from "react-router";
 
 const Register = () => {
 
-    const navigate = useNavigate();
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         // Handle register logic here
