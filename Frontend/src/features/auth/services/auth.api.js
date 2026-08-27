43cc213984fe2a94ac7e86d4dca7f3c1ca657edb
Escape <fardeenhasan57@gmail.com>
2026-08-27 23:12:36 +0530

    ..

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
+const api = axios.create({
+    baseURL: "http://localhost:3000",
+    withCredentials:true
+})
+
 
 export async function register({username, email, password}) {
 
     try {
-        const response = await axios.post('http://localhost:3000/api/auth/register',{
+        const response = await api.post('/api/auth/register',{
         username, email, password
-        },{
-            withCredentials: true
         })
 
         return response.data
@@ -21,9 +24,9 @@ export async function login({email, password}) {
 
     try{
 
-        const response = await axios.post('http://localhost:3000/api/auth/login',{
+        const response = await api.post('/api/auth/login',{
             email, password
-        }, {withCredentials:true})
+        })
 
         return response.data
 
@@ -38,7 +41,7 @@ export async function logout(){
 
     try {
 
-        const response = await axios.get("http://localhost:3000/api/auth/logout",{withCredentials:true})
+        const response = await api.get("/api/auth/logout")
 
         return response.data
 
@@ -51,7 +54,7 @@ export async function logout(){
 export async function getMe() {
     try {
 
-        const response = await axios.get("http://localhost:3000/api/auth/get-me", {withCredentials:true})
+        const response = await api.get("/api/auth/get-me")
 
         return response.data
 
