c3cfa6cdea37ad3b75702324eac567259fe3b861
Escape <fardeenhasan57@gmail.com>
2026-08-28 00:29:41 +0530

    hooks

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe} from "../services/auth.api"
+
+export const useAuth = () => {
+    
+    const context = useContext(AuthContext)
+    const {user, setUser, loading, setLoading} = context
+
+    const  handleLogin = async ({email, password}) => {
+        setLoading(true)
+        const data = await login({email, password})
+        setUser(data.user)
+        setLoading(false)
+    }
+
+    const handleRegister = async ({username, email, password}) => {
+        setLoading(true)
+        const data = await register({username, email, password})
+        setUser(data.user)
+        setLoading(false)
+    }
+}
