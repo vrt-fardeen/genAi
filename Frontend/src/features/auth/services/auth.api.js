f660710f29f1ad0ca6a6371bc40140beaa22f809
Escape <fardeenhasan57@gmail.com>
2026-08-27 23:05:49 +0530

    ..

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -32,3 +32,30 @@ export async function login({email, password}) {
     }
 
 }
+
+
+export async function logout(){
+
+    try {
+
+        const response = await axios.get("http://localhost:3000/api/auth/logout",{withCredentials:true})
+
+        return response.data
+
+    }catch(err) {
+        console.log(err)
+    }
+
+}
+
+export async function getMe() {
+    try {
+
+        const response = await axios.get("http://localhost:3000/api/auth/get-me", {withCredentials:true})
+
+        return response.data
+
+    }catch(err) {
+        console.log(err)
+    }
+}
