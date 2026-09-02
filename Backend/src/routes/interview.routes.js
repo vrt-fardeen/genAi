b814ba524c6c4c5da0d29005f56157068794cefc
Escape <fardeenhasan57@gmail.com>
2026-09-02 22:41:24 +0530

    AI controller

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const authMiddleware = require("../middlewares/auth.middleware.js")
 
 
 const interviewRouter = express.Router();
+
+/**
+ * @route POST /api/interview/
+ * @description generate new interview report of the basis of user self description, resume pdf and job description
+ * @access Private
+ */
+interviewRouter.post('/', authMiddleware.authUser)
+
 
 
 module.exports = interviewRouter
